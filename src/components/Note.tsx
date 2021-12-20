d52fa02a4fe22c5293524d3ebd656a96bfcecb45
Ted Ribeiro <tedribeiro@gmail.com>
2021-12-19 23:40:47 -0300

    feat(Note): create component

---
+++ src/components/Note.tsx
@@ -0,0 +1,27 @@
+import { NoteModel } from "../models/Models";
+
+const Note = (props: NoteModel) => {
+    const { title, subtitle, updatedAt, createdAt } = props;
+
+    return (
+        <div className='p-6 
+                        rounded 
+                        bg-orange-200
+                        flex 
+                        flex-col'>
+            <div>
+                <h3 className="font-bold text-xl">{title}</h3>
+                <p className="text-sm">{subtitle}</p>
+            </div>
+            <div>
+                {
+                    <small className="text-xs">
+                        {updatedAt ? 'Updated' : 'Created'} {updatedAt ?? createdAt}
+                    </small>
+                }
+            </div>
+        </div>
+    );
+}
+
+export default Note;
--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,7 +1,17 @@
+import { NotesListModel } from "../models/Models";
+import Note from "./Note";
+
+const notesMock: NotesListModel = [
+    {id: 1, title: 'Note 1', subtitle: 'Subtitle 1', content: 'Content 1', createdAt: '19/12/2021 23:36', updatedAt: '19/12/2021 23:36'},
+    {id: 2, title: 'Note 2', subtitle: 'Subtitle 2', content: 'Content 2', createdAt: '19/12/2021 23:36'},
+    {id: 3, title: 'Note 3', subtitle: 'Subtitle 3', content: 'Content 3', createdAt: '19/12/2021 23:36'},
+    {id: 4, title: 'Note 4', subtitle: 'Subtitle 4', content: 'Content 4', createdAt: '19/12/2021 23:36', updatedAt: '19/12/2021 23:36'},
+    {id: 5, title: 'Note 5', subtitle: 'Subtitle 5', content: 'Content 5', createdAt: '19/12/2021 23:36'},
+]
 
 const NotesList = () => {
-    return <div>
-        NotesListComponent
+    return <div className="grid gap-2 grid-cols-3">
+        {notesMock.map(n => <Note key={n.id.toString()} {...n} />)}
     </div>
 }
 
