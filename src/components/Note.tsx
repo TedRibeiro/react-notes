cccac8e2116c6aab232ad47ff2c1969ece51fdbb
Ted Ribeiro <tedribeiro@gmail.com>
2021-12-20 00:10:32 -0300

    feat(Note): create edit and delete buttons

--- src/components/Note.tsx
+++ src/components/Note.tsx
@@ -1,3 +1,5 @@
+import { DeleteOutlined, EditOutlined } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 import { NoteModel } from "../models/Models";
 
 const Note = (props: NoteModel) => {
@@ -6,10 +8,17 @@ const Note = (props: NoteModel) => {
     const viewNote = () => {
         console.log('viewNote')
     }
+    const editNote = () => {
+        console.log('editNote')
+    }
+    const deleteNote = () => {
+        console.log('deleteNote')
+    }
 
     return (
-        <div onClick={viewNote} 
-             className="p-6 
+        <div className="relative group">
+            <div onClick={viewNote}
+                className="p-6 
                         rounded 
                         bg-orange-200
                         hover:cursor-pointer 
@@ -17,16 +26,29 @@ const Note = (props: NoteModel) => {
                         flex 
                         flex-col
                         min-h-[12rem]">
-            <div>
-                <h3 className="font-bold text-xl">{title}</h3>
-                <p className="text-sm">{subtitle}</p>
+                <div>
+                    <h3 className="font-bold text-xl">{title}</h3>
+                    <p className="text-sm">{subtitle}</p>
+                </div>
+                <div>
+                    {
+                        <small className="text-xs">
+                            {updatedAt ? 'Updated' : 'Created'} {updatedAt ?? createdAt}
+                        </small>
+                    }
+                </div>
             </div>
-            <div>
-                {
-                    <small className="text-xs">
-                        {updatedAt ? 'Updated' : 'Created'} {updatedAt ?? createdAt}
-                    </small>
-                }
+            <div className="absolute 
+                            top-2 
+                            right-2 
+                            sm:hidden
+                            group-hover:block">
+                <IconButton onClick={editNote}>
+                    <EditOutlined />
+                </IconButton>
+                <IconButton onClick={deleteNote}>
+                    <DeleteOutlined />
+                </IconButton>
             </div>
         </div>
     );
