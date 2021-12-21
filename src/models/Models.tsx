447f252021b67efa7b9ad9e5b90871b5a2bfdb8d
Ted Ribeiro <tedribeiro@gmail.com>
2021-12-20 21:51:21 -0300

    feat: create model for editing

--- src/models/Models.tsx
+++ src/models/Models.tsx
@@ -6,9 +6,19 @@ export interface NoteModel {
     updatedAt?: string,
     createdAt: string
 }
-export interface NotesListModel extends Array<NoteModel>{};
+export interface NotesListModel extends Array<NoteModel> { };
 
 export interface NoteViewModel extends NoteModel {
     open: boolean,
     setOpen: () => void
-  }
+}
+
+export interface NoteEditModel {
+    id?: number,
+    title?: string,
+    subtitle?: string,
+    content?: string,
+    isEdit?: boolean,
+    open: boolean,
+    setOpen: () => void
+}
