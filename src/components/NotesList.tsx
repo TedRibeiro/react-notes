3a0e7f290e076465fd9606897fc1fe0e66ac5bb4
Ted Ribeiro <tedribeiro@gmail.com>
2021-12-20 22:40:03 -0300

    feat: use service for listing

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,23 +1,15 @@
 import { NotesListModel } from "../models/Models";
+import { getAllNotes } from "../services/NoteService";
 import AddNoteButton from "./AddNoteButton";
 import Note from "./Note";
 
-let notesMock: NotesListModel = [
-    { id: 1, title: 'Note 1', subtitle: 'Subtitle 1', content: 'Content 1', createdAt: '19/12/2021 23:36', updatedAt: '19/12/2021 23:36' },
-    { id: 2, title: 'Note 2', subtitle: 'Subtitle 2', content: 'Content 2', createdAt: '19/12/2021 23:36' },
-    { id: 3, title: 'Note 3', subtitle: 'Subtitle 3', content: 'Content 3', createdAt: '19/12/2021 23:36' },
-    { id: 4, title: 'Note 4', subtitle: 'Subtitle 4', content: 'Content 4', createdAt: '19/12/2021 23:36', updatedAt: '19/12/2021 23:36' },
-    { id: 5, title: 'Note 5', subtitle: 'Subtitle 5', content: 'Content 5', createdAt: '19/12/2021 23:36' },
-]
-
-//notesMock = [];
-
 const NotesList = () => {
-    if (notesMock.length > 0) {
+    const notes = getAllNotes()
+    if (notes.length > 0) {
         return (
             <div className="grid gap-2 grid-cols-3">
                 <AddNoteButton />
-                {notesMock.map(n => <Note key={n.id.toString()} {...n} />)}
+                {notes.map(n => <Note key={n.id.toString()} {...n} />)}
             </div>
         );
     } else {
